760a1412c7498c896b50bfabac0cda111d40e47d
= <=>
2025-12-13 11:11:47 +0530

    register otp checking

--- app.js
+++ app.js
@@ -53,7 +53,10 @@ io.on("connection", (socket) => {
     });
 });
 
-app.use(cors({ origin: "*" }));
+app.use(cors({
+    origin: ["http://localhost:5173", "https://hms-frontend-green.vercel.app", "http://localhost:3000"],
+    credentials: true
+}));
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
